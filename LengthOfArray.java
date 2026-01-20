a9c3dffd15e91575dc7a5263b766aa9fe097cd57
HIMANSHU CHATURVEDI <himanshuchaturvedi2006@gmail.com>
2026-01-21 00:07:11 +0530

    Create LengthOfArray.java

---
+++ LengthOfArray.java
@@ -0,0 +1,8 @@
+public class LengthOfArray {
+    public static void main(String[] args) {
+       int length[]= new int [50];
+       System.out.println("lenghtofarray :- " + length.length);
+
+    }
+    
+}
