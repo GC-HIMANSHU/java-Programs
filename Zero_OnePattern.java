ede51b405b0260ae6eba7196226f2e964a648fe8
HIMANSHU CHATURVEDI <himanshuchaturvedi2006@gmail.com>
2026-01-22 00:09:18 +0530

    Create Zero_OnePattern.java

---
+++ Zero_OnePattern.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Zero_OnePattern {
+public static void zero_onepatterns(int n) {
+    //outer loop
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if((i+j)%2==0){
+                System.out.print(1);
+
+            }
+            else{
+                System.out.print(0);
+            }
+            
+        }
+        System.out.println();
+    }
+
+}
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        zero_onepatterns(n);
+        
+    }
+}
+
+    
+
