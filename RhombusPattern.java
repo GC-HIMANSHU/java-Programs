81b2fc80883f15b47938e68fee6675b13920f647
HIMANSHU CHATURVEDI <himanshuchaturvedi2006@gmail.com>
2026-01-23 00:48:53 +0530

    Create RhombusPattern.java

---
+++ RhombusPattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class RhombusPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+            System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+    }
+}
