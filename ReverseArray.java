72878342191c37bbba9da0278a06fb4597093d79
HIMANSHU CHATURVEDI <himanshuchaturvedi2006@gmail.com>
2026-01-24 00:03:22 +0530

    Create ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,29 @@
+
+    import java.util.SortedMap;
+
+public class ReverseArray {
+    public static void reverse(int numbers[]){
+        int start = 0;
+        int last = numbers.length - 1;
+        while(start < last){
+            int temp = numbers[last];
+            numbers[last] = numbers[start];
+            numbers[start] = temp;
+            start ++;
+            last --;
+
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4,5,6,7,8,95,4,3,5,7,7,4,3,5,7,3,5};
+
+        reverse(numbers);
+       for(int i=0; i<=numbers.length;i++){
+          System.out.print(" " + numbers[i]);
+       }
+        System.out.println();
+    }
+    
+}
+
+
