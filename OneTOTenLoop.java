e3664e7f188a362535dfb597078f74e9fb10399d
HIMANSHU CHATURVEDI <himanshuchaturvedi2006@gmail.com>
2026-01-28 19:21:17 +0530

    Create OneTOTenLoop.java

---
+++ OneTOTenLoop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class OneTOTenLoop {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        while(a<=10){
+            System.out.print(a);
+            a++;
+        }
+    }
+
+    
+}
